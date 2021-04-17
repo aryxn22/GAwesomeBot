ffaa7e9876ecb056dfc027444519475d2a04663c
aryxn22 <64898846+aryxn22@users.noreply.github.com>
2021-04-17 23:34:03 +0530

    Delete public directory

--- public/say.js
+++
@@ -1,9 +0,0 @@
-module.exports = async ({ Constants: { Colors } }, documents, msg) => {
-	msg.send({
-		embed: {
-			color: Colors.SUCCESS,
-			description: `${msg.suffix && msg.suffix !== "" ? msg.suffix : "🙊"}`,
-		},
-		disableEveryone: true,
-	});
-};
